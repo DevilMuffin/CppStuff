00a83567390c6e39fb9f1f4c62ff3efd202a173d
Devilmuffin <94016701+DevilMuffin@users.noreply.github.com>
2025-09-09 11:05:15 +0000

    setup main more

---
+++ mainMore.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int x;
+
+
+    cout << "Enter a number: ";
+    cin >> x;
+    
+    for(int i = 0; i<x; i++) {
+        for(int y = 0; y<x-i; y++) {
+            cout << " ";
+        }
+        for(int z = 0; z<i+1; z++) {
+            cout << "#";
+        }
+
+        cout << endl; 
+    }
+
+    return 0;
+}
